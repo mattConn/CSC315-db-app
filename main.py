3239e99c48dea8c060740262003479a7d08a77e1
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-12-08 23:06:51 -0500

    inserting and selecting from db via python and param. query

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import sys
 
+# connect to db as app user
 try:
     db  = mysql.connector.connect(
         host='localhost',
@@ -11,11 +12,19 @@ except:
     print('Error connecting to database.')
     sys.exit(1)
 
-
 cursor = db.cursor()
 
-cursor.execute('select name,seq from Sequences;')
-for i in cursor:
-    print(i[0]) # name
-    for j in i[1].split(', '): print(j)
-# cursor.execute('select * from Sequences;')
+# insert a new integer sequence (powers of 2)
+cursor.execute(f'insert into Sequences values(5, \'Powers of 2\', \'{str([2**i for i in range(30)])[1:-1]}\')') # mean one-liner
+
+# select parametrized query 
+selectSeq = 'select %s from Sequences where %s;'
+
+# select pi digits
+cursor.execute(selectSeq % ('seq','name = \'Pi Digits\'') )
+
+# print results
+for i in cursor: print(i)
+
+db.commit() # make changes stick 
+db.close()
