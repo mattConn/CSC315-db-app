4c4e6b09f4da4fcc3b91283ac9f970c9d2d7bc38
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-12-09 19:02:40 -0500

    inserting into db via url & querystring

--- server.py
+++ server.py
@@ -14,9 +14,23 @@ def index():
 @app.route("/viewseq")
 def viewSeq():
 
+    name = request.args.get("name")
     response = Response()
-    cursor.execute(f'select seq from Sequences where name=\'{request.args.get("name")}\';')
+    cursor.execute(f'select seq from Sequences where name=\'{name}\';')
     response.set_data(str([str(i) for i in cursor]))
+
+    return response
+
+@app.route("/addseq")
+def addSeq():
+
+    id = request.args.get("id")
+    name = request.args.get("name")
+    seq = request.args.get("seq")
+
+    response = Response()
+    cursor.execute(f'insert into Sequences values({id},\'{name}\',\'{seq}\');')
+    response.set_data(f'Added sequence {name}.')
 
     return response
 
