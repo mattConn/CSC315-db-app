de962c91209f0839d24fa41590c35cf46835ee89
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-12-09 19:16:12 -0500

    added comments to server script

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from db import cursor
 
 app = Flask(__name__)
 
+# display all integer sequence names"
 @app.route("/")
 def index():
     cursor.execute('select name from Sequences;')
@@ -12,26 +13,29 @@ def index():
 
     return response
 
+# view a sequence. e.g.: /viewseq?name=pi digits
 @app.route("/viewseq")
 def viewSeq():
 
-    name = request.args.get("name")
+    name = request.args.get("name") # get query string
     response = Response()
     cursor.execute(f'select seq from Sequences where name=\'{name}\';')
-    response.set_data(str([str(i) for i in cursor]))
+    response.set_data(str([str(i) for i in cursor])) # write to response
 
     return response
 
+# add a sequence. e.g.: /addseq?id=1337&name=Natural Numbers&seq=1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 @app.route("/addseq")
 def addSeq():
 
+    # get id, name and seq query strings
     id = request.args.get("id")
     name = request.args.get("name")
     seq = request.args.get("seq")
 
     response = Response()
     cursor.execute(f'insert into Sequences values({id},\'{name}\',\'{seq}\');')
-    response.set_data(f'Added sequence {name}.')
+    response.set_data(f'Added sequence {name}.') # write response with new sequence name
 
     return response
 
